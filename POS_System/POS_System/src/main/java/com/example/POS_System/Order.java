92d8a0def24af2c313b71675be6ee824f8709954
Ratinder Jit <ratinderjit176@gmail.com>
2025-01-03 15:36:03 -0400

    order, orderController, and orderRepo added and functioning

---
+++ POS_System/POS_System/src/main/java/com/example/POS_System/Order.java
@@ -0,0 +1,61 @@
+package com.example.POS_System;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long orderId;
+
+    private LocalDate orderDate;
+
+    private BigDecimal totalAmount = BigDecimal.ZERO;
+
+    public Order() {
+    }
+
+    public Order(LocalDate orderDate, BigDecimal totalAmount) {
+        this.orderDate = orderDate;
+        this.totalAmount = totalAmount;
+    }
+
+    public long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(long orderId) {
+        this.orderId = orderId;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public BigDecimal getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(BigDecimal totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId=" + orderId +
+                ", orderDate=" + orderDate +
+                ", totalAmount=" + totalAmount +
+                '}';
+    }
+}
+
---
+++ POS_System/POS_System/src/main/java/com/example/POS_System/OrderController.java
@@ -0,0 +1,51 @@
+package com.example.POS_System;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @PostMapping
+    public Order addOrder(@RequestBody Order order){
+        order.setOrderDate(LocalDate.now());
+        return orderRepository.save(order);
+    }
+
+    @GetMapping
+    public List<Order> getAllOrders(){
+        return orderRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Order getOrderById(@PathVariable long id){
+        return orderRepository.findById(id).orElseThrow(() -> new RuntimeException("Order not found!"));
+    }
+
+    @PutMapping("/{id}")
+    public Order updateOrder(@PathVariable long id, @RequestBody Order updatedOrder){
+        Order order = orderRepository.findById(id).orElseThrow(() -> new RuntimeException("Order not found!"));
+        order.setOrderDate(updatedOrder.getOrderDate());
+        order.setTotalAmount(updatedOrder.getTotalAmount());
+        return orderRepository.save(order);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteOrder(@PathVariable long id){
+        orderRepository.deleteById(id);
+    }
+
+
+
+}
